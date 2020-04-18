ca2b2220de501fb6d32d3043f43664e7e65d26d8
Soheib Bettahar <soheibbett@gmail.com>
2020-04-19 00:41:58 +0100

    added roject general structure and firebase connection

---
+++ app/src/main/java/com/ssquare/myapplication/monokrome/login/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.ssquare.myapplication.monokrome.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ssquare.myapplication.monokrome.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/ssquare/myapplication/monokrome/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.ssquare.myapplication.monokrome.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ssquare.myapplication.monokrome.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

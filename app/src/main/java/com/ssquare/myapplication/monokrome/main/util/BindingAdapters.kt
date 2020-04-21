15a4c8619e5d6c28ec160f191ac2d13c1f057bb5
Soheib Bettahar <soheibbett@gmail.com>
2020-04-21 22:53:37 +0100

    tweaked bindingAdapter

--- app/src/main/java/com/ssquare/myapplication/monokrome/main/util/BindingAdapters.kt
+++ app/src/main/java/com/ssquare/myapplication/monokrome/main/util/BindingAdapters.kt
@@ -3,13 +3,11 @@ package com.ssquare.myapplication.monokrome.main.util
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
-import com.ssquare.myapplication.monokrome.R
 
 
 @BindingAdapter("imageUrl")
 fun loadImage(view: ImageView, url: String) {
     Glide.with(view.context).load(url)
-        .placeholder(R.drawable.common_google_signin_btn_icon_dark_normal_background)
         .centerCrop()
         .into(view)
 }
